282a900054643b200e670bb534d02016c8d58ea9
Haseeb1389446 <haseeb1389446@gmail.com>
2025-03-23 05:20:37 -0700

    ASP.NET

--- CODE CRITERIA/ASP .NET/Template_Integration/Template_Integration/Controllers/AuthController.cs
+++ CODE CRITERIA/ASP .NET/Template_Integration/Template_Integration/Controllers/AuthController.cs
@@ -1,21 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Template_Integration.Models;
 
 namespace Template_Integration.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult SignUp()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult SignUp(SignUpViewModel model)
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return View(model);
+                if (!(await _roleManager.RoleExistsAsync("User"))) {
+                    IdentityRole role = new()
+                    {
+                        Name = "User"
+                    };
+
+                    await _roleManager.CreateAsync(role);
+                }
+
+                if (!(await _roleManager.RoleExistsAsync("Admin")))
+                {
+                    IdentityRole role = new()
+                    {
+                        Name = "Admin"
+                    };
+
+                    await _roleManager.CreateAsync(role);
+                }
+
+                IdentityUser user = new()
+                {
+                    UserName = model.UserName,
+                    Email = model.UserEmail,
+                };
+
+                await _userManager.CreateAsync(user);
+
+                if (model.UserEmail == "admin@gmail.com") {
+                    await _userManager.AddToRoleAsync(user, "Admin");
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, "User");
+                }
+
+                return Redirect("login");
             }
 
             return View(model);
