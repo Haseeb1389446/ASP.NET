9b053edaedae22c16b651664258f87b8b5cca939
Haseeb1389446 <haseeb1389446@gmail.com>
2025-05-05 04:03:43 -0700

    Institute Of Fine Arts

---
+++ CODE CRITERIA/PROJECTS/Institute_Of_Fine_Arts/Institute_Of_Fine_Arts/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+using Institute_Of_Fine_Arts.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Institute_Of_Fine_Arts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Competitions()
+        {
+            return View();
+        }
+
+        public IActionResult Exhibitions()
+        {
+            return View();
+        }
+
+        public IActionResult Awards()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        public IActionResult Staff()
+        {
+            return View();
+        }
+
+        public IActionResult Manager()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ CODE CRITERIA/PROJECTS/Institute_Of_Fine_Arts/Institute_Of_Fine_Arts/Models/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Institute_Of_Fine_Arts.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? UserEmail { get; set; }
+
+        [Required]
+        public string? UserPassword { get; set; }
+    }
+}
